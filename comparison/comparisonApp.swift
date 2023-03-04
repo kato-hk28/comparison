36b0b0030ede1fbc0883b8a3127e3e0a35079229
kato-hk28 <taihuijiateng870@gmail.com>
2023-03-04 23:03:18 +0900

    Initial Commit

---
+++ comparison/comparisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  comparisonApp.swift
+//  comparison
+//
+//  Created by Kato Taiki on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct comparisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
